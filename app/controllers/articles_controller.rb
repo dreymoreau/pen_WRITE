d4c1459924f9cc09494eb0d97ef836847781ec76
dreymotoh <striclyspoken@gmail.com>
2024-10-28 12:13:57 -0400

    commented out ArticlesController

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 
-# this controller is inheriting everything from the ApplicationController
+# this controller is inheriting everything from the ApplicationController without having to do require "application_controller" # DON'T DO THIS.
 
 class ArticlesController < ApplicationController
   # The index action is empty. When an action does not explicitly render a view (or otherwise trigger an HTTP response), Rails will automatically render a view that matches the name of the controller and action. Convention Over Configuration! Views are located in the app/views directory. So the index action will render app/views/articles/index.html.erb by default.
