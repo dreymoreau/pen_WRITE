d9e11999a7ed14ef2440c69a39876f829312caec
dreymotoh <striclyspoken@gmail.com>
2024-11-01 12:13:57 -0400

    changed log controller to devlog,, updated methods in log method but kept instance as log

--- app/controllers/devlog.rb
+++ app/controllers/devlog.rb
@@ -1,10 +1,10 @@
-class Log < ArticlesController
+class DevLog < ArticlesController
   # def index
   # fetching all articles form the database using the controller instance variable
   # also!! Article.all is the method you used in the console to query everything from the database
   # @articles = Article.all
   # end
   def log
-     @log = Log.all
+     @log = DevLog.all
   end
 end
