1a96789be76efa2a60043b4fa2a5b6215e9e9e55
dreymotoh <striclyspoken@gmail.com>
2024-11-02 11:08:33 -0400

    FIXED LOG BUG!!!!!! everything is working and being rendered now,, statically and dynamically

--- app/controllers/log_controller.rb
+++ app/controllers/log_controller.rb
@@ -1,10 +1,5 @@
 class LogController < ApplicationController
-  # def index
-  # fetching all articles form the database using the controller instance variable
-  # also!! Article.all is the method you used in the console to query everything from the database
-  # @articles = Article.all
-  # end
-  def log
-     @log = Log.all
+  def index
+    @log = Log.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # the root route is also mapped to the index action of ArticlesController.
-  root "articles#index"
+  root "log#index"
 
-  get "/articles", to: "articles#index"
-  get "/log", to: "logs#show"
+  # get "/articles", to: "articles#index"
+  get "/log", to: "log#index"
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
