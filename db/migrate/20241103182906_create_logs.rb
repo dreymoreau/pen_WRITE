e755cab04130c5bd28e354bc8e79a26f35eb19a8
dreymotoh <striclyspoken@gmail.com>
2024-11-04 13:30:29 -0500

    big day of bug squashing as well as understanding concepts better!!!! routes all working,, updating the database and using it for its corresponding view,, offfff the rails

--- db/migrate/20241103182906_create_logs.rb
+++ db/migrate/20241103182906_create_logs.rb
@@ -1,5 +1,6 @@
 class CreateLogs < ActiveRecord::Migration[7.2]
   def change
+      # creating table thats called logs with the generated key values date and body,, timestamps is createdat and updated at
       create_table :logs do |t|
         t.string :date
         t.text :body
--- db/migrate/20241103185340_rename_title_to_date_in_logs.rb
+++ db/migrate/20241103185340_rename_title_to_date_in_logs.rb
@@ -1,5 +1,6 @@
 class RenameTitleToDateInLogs < ActiveRecord::Migration[7.2]
   def change
+        # for keeping the database consistency in environments where the logs table was already created, and you need to rename the existing column for all database instances (e.g., production, staging, or other developers' environments).
         rename_column :logs, :title, :date
   end
 end
